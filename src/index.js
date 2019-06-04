b2781cda55839099e30d9e7721a5f358132267dd
mi19her <milcaph.19@gmail.com>
2019-06-04 13:06:40 -0500

    carpeta img

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-/* Acá va tu código */
+const claveCorecta= "LABORATORIA";
+let intento = 0;
+//funcion lee clave corecta 
+ingreso.addEventListener('click',()=>{
+const clave= document.getElementById("secreto").value;
+if (clave.value == claveCorecta){
+        document.getElementById('si').innerHTML = "CLAVE CORECTA";
+        document.getElementById('page1').classList.add('hide');
+        document.getElementById('mensaje').classList.remove('hide');
+} //segundo intento clave incorecta
+else{
+    if ( intento<2){
+        document.getElementById('si').innerHTML = "INTENTA DE NUEVO MÁS TARDE";
+        clave.value='';
+        intento++;
+    } else{
+        document.getElementById('si').innerHTML = "vuelve mas tarde";
+    }
+}
+})
