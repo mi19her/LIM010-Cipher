4fab306e25abc98007f0252e3ff7ee7d3ac6a8b0
mi19her <milcaph.19@gmail.com>
2019-06-10 11:54:54 -0500

    segundo avance

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,30 @@
 window.cipher = {
-  encode: (offset, string) => {
+  encode: (numero, texto) => {
     /* Acá va tu código que cifra*/
+    let textoCodificado = " ";
+    
+    for (let i = 0; i < texto.length; i++) {
+      let l = texto.charCodeAt(i);
+      let letra = texto.charAt(i);
+      textoCodificado += String.fromCharCode((l - 65 + numero) % 26 + 65);
+      if (letra == " ") {
+        textoCodificado += " ";
+      }
+    }
+    return textoCodificado;
   },
-  decode: (offset, string) => {
+  decode: (numero, texto) => {
     /* Acá va tu código que descifra*/
+    let textoCodificado=" ";
+    let i;
+    for( i=0; i<texto.length; i++){
+      let l= texto.charCodeAt(i);
+      let letra= texto.charAt(i);
+      textoCodificado += String.fromCharCode((l+65-numero)%26 + 65);
+      if(letra == "  "){
+        textoCodificado +="   ";
+      }
+      }
+      return textoCodificado;
   }
 };
--- src/index.js
+++ src/index.js
@@ -10,8 +10,8 @@ if (clave === claveCorecta){
         document.getElementById('page3').classList.add('hide');
 } //segundo intento clave incorecta
 else{
-    if ( intento<=2){
-        document.getElementById('si').innerHTML = "INTENTA DE NUEVO";
+    if ( intento<2){
+        document.getElementById('si').innerHTML = " ERROR INTENTA DE NUEVO";
         clave.value='';
         intento++;
     } else{
@@ -23,44 +23,38 @@ else{
 cifrar.addEventListener('click',()=>{
     const texto= document.getElementById('ingresaTexto').value.toUpperCase();
     const numero= parseInt(document.getElementById('escala').value);
-    let textoCodificado=" ";
-    let i;
-    for( i=0; i<texto.length; i++){
-      let l= texto.charCodeAt(i);
-      let letra =texto.charAt(i);
-      textoCodificado += String.fromCharCode((l-65+numero)%26 + 65);
-      if(letra == " "){
-        textoCodificado +=" ";      
-      }
-      }  
+    //conexion con el cipher
+   let result = cipher.encode(numero, texto);
     document.getElementById('page3').classList.remove('hide');
     document.getElementById('page2').classList.add('hide');
-    document.getElementById('resultado').innerHTML=textoCodificado;
-    
+    document.getElementById('resultado').value=result;
     
 });
 descifrar.addEventListener('click',()=>{
     const texto= document.getElementById('ingresaTexto').value.toUpperCase();
     const numero= parseInt(document.getElementById('escala').value);
-    let textoCodificado=" ";
-    let i;
-    for( i=0; i<texto.length; i++){
-      let l= texto.charCodeAt(i);
-      let letra= texto.charAt(i);
-      textoCodificado += String.fromCharCode((l-65-numero)%26 + 65);
-      if(letra == " "){
-        textoCodificado +=" ";
-      }
-      }
+    //conexion con el cipher
+    let result = cipher.decode(numero,texto);
     document.getElementById('page3').classList.remove('hide');
     document.getElementById('page2').classList.add('hide');
-    document.getElementById('resultado').innerHTML=textoCodificado;
+    document.getElementById('resultado').value=result;
 });
 
-copiar.addEventListener('click',()=>{
-    document.fin.resultado.select();
-    window.clipboardData.setData("text", document.fin.resultado.value);
+copiar.addEventListener('click',(evento)=>{
+    evento.preventDefault();
+    const TextoCop= document.getElementById('resultado');
+    TextoCop.select();
+    document.execCommand('copy'); 
 }) 
 
+volver.addEventListener('click',(evento)=>{
+    document.getElementById('ingresaTexto').value =' ';
+    document.getElementById('resultado').value=' ';
+    document.getElementById('escala').value=' ';
+    evento.preventDefault();
+    document.getElementById('page1').classList.add('hide');
+    document.getElementById('page3').classList.add('hide');
+    document.getElementById('page2').classList.remove('hide');
+})
   
 
